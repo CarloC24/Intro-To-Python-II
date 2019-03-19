79f03b85a290900e4c0902a958a41e3e6138d3eb
CarloC24 <carlo.clamucha@gmail.com>
2019-03-18 21:19:18 -0500

    added player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self,startLocation):
+        self.location = startLocation
+
+    def change_location(self,changedLocation):
+        self.location = changedLocation
+
